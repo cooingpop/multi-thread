817c612600ae4e0ee71ec23a9b31acd46be2a1e9
PLAYD\junyoung.park <junyoung.park@playd.com>
2024-01-19 16:51:13 +0900

    etc: 종료 메서드 수정

--- src/main/java/com/wywta/MultiThreadApplication.java
+++ src/main/java/com/wywta/MultiThreadApplication.java
@@ -38,24 +38,18 @@ public class MultiThreadApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        // 애플리케이션 시작 시 실행할 작업들
-        // 종료 훅 등록
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            // 애플리케이션 종료 시 실행할 작업들
-            cleanup();
-            // 안전하게 애플리케이션 종료
-            collectorService.shutdown();
-            SpringApplication.exit(applicationContext, () -> 0);
-        }));
-
-        // CollectService의 execute 메서드 호출
-        log.info("start");
+        // 애플리케이션 종료 시 실행할 작업들
+        Runtime.getRuntime().addShutdownHook(new Thread(this::cleanup));
+        log.info("MultiThreadApplication start");
         collectorService.execute();
     }
 
     private void cleanup() {
         // 리소스 해제 및 정리 작업 수행
-        System.out.println("Cleaning up resources...");
+        log.info("MultiThreadApplication Cleaning up resources...");
+        // 안전하게 애플리케이션 종료
+        collectorService.shutdown();
+        SpringApplication.exit(applicationContext, () -> 0);
     }
 }
 
